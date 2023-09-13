38b6e591b161fc8bff676eb230ac7ec84f1bf75b
Timothy Olili <timmolili27@gmail.com>
2023-09-13 15:27:48 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	 let obj = {};
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str.charAt(i);
+
+    if (obj[char] != undefined) {
+      obj[char]++;
+    } else {
+      obj[char] = 1;
+    }
+  }
+
+  for (const iterator in obj) {
+    // console.log(iterator);
+    if (obj[iterator] == 1) return iterator;
+  }
+
+  return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
